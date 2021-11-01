209103e9d3079d7c3ca14bbe98ad6f4efffe87c4
unknown <dwny3d.amg@gmail.com>
2021-11-01 15:47:10 +0100

    first commit

---
+++ Assets/script/CamMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController  : MonoBehaviour
+{
+    public Transform[] views;
+    public float transitionSpeed;
+    Transform currentView;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
